f64340a58a535e613f3e675b028528285d8e6cbb
Cao Bittencourt <caobittencourt@gmail.com>
2023-09-27 19:53:22 -0300

    moved execution to exec_package_versions.sh

--- code/exec_package_versions.sh
+++ code/exec_package_versions.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
-# Change directory to GitHub
-cd /c/Users/CAO/Documents/GitHub/
+# Change directory to code
+cd /c/Users/CAO/Documents/GitHub/auto.cv/code
 
-# Setup contrab job to execute package version script
-contrab -e
+# # Setup contrab job to execute package version script
+# contrab -e
 
-# 1: Minutes (0-59)
-# 2: Hours (0-23)
-# 3: Days (1-31)
-# 4: Month (1-12)
-# 5: Day of the week(1-7)
+# # 1: Minutes (0-59)
+# # 2: Hours (0-23)
+# # 3: Days (1-31)
+# # 4: Month (1-12)
+# # 5: Day of the week(1-7)
 
-* * * * 6 /get_package_versions.sh
-# 0.5 * * * * /get_package_versions.sh
+# * * * * 6 /get_package_versions.sh
+# # 0.5 * * * * /get_package_versions.sh
+
+# Change directory to code
+cd /c/Users/CAO/Documents/GitHub/auto.cv/code
+
+# Run bash script to update package_versions.csv
+bash get_package_versions.sh
+
+# Run R script to generate LaTeX input
+Rscript sketch_packages.R
--- code/get_package_versions.sh
+++ code/get_package_versions.sh
@@ -22,9 +22,3 @@ sed -i -e "s/$/,R/" package_versions.csv
 
 # Add variable names to beginning of .csv file
 sed -i "1s/^/package_name,package_subtitle,version,programming_language \n/" package_versions.csv
-
-# Change directory to code
-cd /c/Users/CAO/Documents/GitHub/auto.cv/code
-
-# Run R script to generate LaTeX input
-Rscript sketch_packages.R
